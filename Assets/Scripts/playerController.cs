af7586fe42eed6df28b19fb9f7cb0af8bd3f63cc
rizzutan <80140803+rizzutan@users.noreply.github.com>
2022-11-24 04:49:37 -0500

    initial multithreading attempt
    oh god, what a horrible commit this is

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class playerController : MonoBehaviour
@@ -8,6 +9,10 @@ public class playerController : MonoBehaviour
     Rigidbody2D rb;
     bool isGrounded, isCollidingRight, isCollidingLeft = false;
     float[] forceToAdd = new float[2] {0.0f, 0.0f};
+    float[] forceToAddMain = new float[2] { 0.0f, 0.0f };
+    float horizontal = 0.0f;
+    float vertical = 0.0f;
+    bool workAround = true;
 
     // Local variables that are editable in inspect menu
     [SerializeField] float speed = 16.0f;
@@ -24,10 +29,11 @@ public class playerController : MonoBehaviour
     // Awake is called on the first frame
     void Awake()
     {
-
+        Thread thread = new Thread(physics);
+        thread.Start();
     }
 
-    // Update is called at fixed intervals
+    // FixedUpdate is called at fixed intervals
     void FixedUpdate()
     {
         // Check if the player is on the ground
@@ -35,40 +41,66 @@ public class playerController : MonoBehaviour
         //Check if the player is colliding with wall on either side
         isCollidingRight = Physics2D.OverlapBox(rightCheck.position, new Vector2(dist, 0.9f), groundMask);
         isCollidingLeft = Physics2D.OverlapBox(leftCheck.position, new Vector2(dist, 0.9f), groundMask);
-        // Create force to allow for player movement
-        forceToAdd[0] = Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime;
+        // Grab player input
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Jump");
+    }
 
-        if ((isCollidingRight && forceToAdd[0] > 0) || (isCollidingLeft && forceToAdd[0] < 0))
+    void Update()
+    {
+        // Grab local copy of forceToAdd
+        lock (forceToAdd)
         {
-            forceToAdd[0] = 0;
-        }
-
-        if (!isGrounded)
-        {
-            forceToAdd[1] = forceToAdd[1] + (-grav * Time.fixedDeltaTime);
-            if (forceToAdd[1] < -grav)
-            {
-                forceToAdd[1] = -grav;
-            }
-
-        }
-        else
-        {
-            forceToAdd[1] = 0.0f;
-        }
-
-        // If player can jump then add force to forceToAdd
-        if (isGrounded && Input.GetAxis("Jump") > 0)
-        {
-            forceToAdd[1] = jump;
-        }
-        // Stop player if there is no input
-        if (Input.GetAxis("Horizontal") == 0)
-        {
-            forceToAdd[0] = 0;
+            forceToAddMain = forceToAdd;
         }
 
         // Apply forces to player
-        transform.position = new Vector3(transform.position.x + forceToAdd[0], transform.position.y + forceToAdd[1], 0);
+        transform.position = new Vector3(transform.position.x + forceToAddMain[0], transform.position.y + forceToAddMain[1], 0);
+    }
+
+    void physics()
+    {
+        while (workAround)
+        {
+            // Create force to allow for player movement
+            forceToAdd[0] = horizontal * speed * (1/1000);
+
+            if ((isCollidingRight && forceToAdd[0] > 0) || (isCollidingLeft && forceToAdd[0] < 0))
+            {
+                forceToAdd[0] = 0;
+            }
+
+            if (!isGrounded)
+            {
+                forceToAdd[1] = forceToAdd[1] + (-grav * (1 / 1000));
+                if (forceToAdd[1] < -grav)
+                {
+                    forceToAdd[1] = -grav;
+                }
+
+            }
+            else
+            {
+                forceToAdd[1] = 0.0f;
+            }
+
+            // If player can jump then add force to forceToAdd
+            if (isGrounded && vertical > 0)
+            {
+                forceToAdd[1] = jump;
+            }
+            // Stop player if there is no input
+            if (horizontal == 0)
+            {
+                forceToAdd[0] = 0;
+            }
+
+            Thread.Sleep(1);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        workAround = false;
     }
 }
